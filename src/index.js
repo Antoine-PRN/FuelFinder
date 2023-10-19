777b1d25a9f61268ce4aa26f3f1d9b855fde6a14
AntoinePR1 <titounet.perrin@gmail.com>
2023-10-19 14:57:02 +0200

    PWA instaurtaion

--- src/index.js
+++ src/index.js
@@ -4,12 +4,21 @@ import App from './App';
 import "./style/styles.css";
 import { Provider } from 'react-redux';
 import Store from './store';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={Store}>
     <React.StrictMode>
+      {/* <head>
+        {linkElement}
+      </head> */}
       <App />
     </React.StrictMode>
   </Provider>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
